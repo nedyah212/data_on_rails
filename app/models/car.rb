8838f516583b92f3b8c5731f132823c07c0a84ab
nedyah212 <hploszay2@academic.rrc.ca>
2025-10-01 10:05:07 -0500

    tested the cars script, made some changes to output

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,22 +5,12 @@ class Car < ApplicationRecord
   has_many :car_features
   has_many :features, through: :car_features
 
-  validates :make, presence: true
+  validates :manufacturer_id, presence: true
   validates :model, presence: true
   validates :color, presence: true
-  validates :engine_size, presence: true
+  validates :trim, presence: true
   validates :year, numericality: {
     only_integer: true, greater_than: 1950,
     less_than_or_equal_to: Date.current.year + 1
   }
-  validates :base_price, numericality: {
-    greater_than_or_equal_to: 0
-  } allow_nil: true
-  validates :kilometers, numericality: {
-    only_integer: true, greater_than_or_equal_to: 0
-  } allow_nil: true
-  validates :transmission, inclusion: {
-    in: ['automatic', 'manual', 'cvt', 'semi-automatic'],
-      message: "%{value} is not a valid transmission"
-    }, allow_nil: true
-    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,12 @@ puts "Created #{Manufacturer.count} manufacturers"
 
 ### Seed Features 2/8
 Feature.destroy_all
-puts "Seeding Car Features with Faker..."
+puts "Seeding Features with Faker..."
 categories = ["Safety", "Entertainment", "Comfort", "Technology", "Exterior", "Performance", "Interior"]
 50.times do |i|
   Feature.create!(
-    name: "#{Faker::Commerce.product_name}",
+    ###This name is a placeholder until i update the feature table to link to the manufacturer
+    name: "#{Faker::Vehicle.manufacture} #{Faker::Commerce.product_name}",
     description: "#{Faker::Marketing.buzzwords}",
     category: categories.sample,
     base_cost: rand(100..3000)
@@ -63,30 +64,35 @@ puts "Created #{Dealership.count} dealerships"
 ### I think this is a good enough reason to use a nested loop
 ### I want to avoid rate limiting, so I will make multiple calls to the API
 Car.destroy_all
-puts "Seeding Cars with Manufacturers, and API"
 COLORS = ['Red', 'Blue', 'Green', 'Black', 'White', 'Silver', 'Yellow', 'Grey', 'Orange', 'Purple']
-
-make = Manufacturer.sample
-year = rand(2000..2025)
 iterations = rand(5..15)
 
+puts "Seeding Cars with Manufacturers, and API"
 iterations.times do
-  url = URI("http://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=#{make}&year=#{year}")
+  make = Manufacturer.all.sample
+  year = rand(2015..2022)
+  url = URI("http://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=#{make.name}&year=#{year}")
+
+  puts "Fetching data from API for #{make.name} for year #{year}"
   response = Net::HTTP.get(url)
   car_data = JSON.parse(response)
+  sleep(0.25)
 
-  if car_data['Trims']
+  if car_data['Trims'].any?
     rand(1..10).times do
       selection = car_data['Trims'].sample
       Car.create(
-        manufacturer_id: manufacturer.id,
+        manufacturer_id: make.id,
         model: selection['model_name'],
         year: selection['model_year'],
         color: COLORS.sample,
         trim: selection['model_trim'],
       )
     end
+    puts "----Created #{Car.where(manufacturer_id: make.id, year: year).count} vehicles"
+  else
+    puts "No data found for #{make.name} for year #{year}"
   end
-  sleep(.25)
+
 end
 puts "Created #{Car.count} cars"

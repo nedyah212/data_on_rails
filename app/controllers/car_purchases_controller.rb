1e564713dc8e724bc17694c3a33df2f09da61731
nedyah212 <hploszay2@academic.rrc.ca>
2025-10-03 09:18:11 -0500

    about page done, with png of erd

--- app/controllers/car_purchases_controller.rb
+++ app/controllers/car_purchases_controller.rb
@@ -2,8 +2,4 @@ class CarPurchasesController < ApplicationController
   def index
     @car_purchases = CarPurchase.includes(:car, :person, :dealership, :salesperson).all
   end
-
-  def show
-    @car_purchase = CarPurchase.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "static/index"
   resources :salespeople
   root 'cars#index'
 
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get 'static/index', to: 'static#index'
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker

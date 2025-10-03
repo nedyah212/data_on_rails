ce41778573c816336f3fc4bffb9534c2af8c360a
nedyah212 <hploszay2@academic.rrc.ca>
2025-10-03 10:45:43 -0500

    pagination working for car purchases

--- app/controllers/car_purchases_controller.rb
+++ app/controllers/car_purchases_controller.rb
@@ -1,5 +1,8 @@
 class CarPurchasesController < ApplicationController
   def index
     @car_purchases = CarPurchase.includes(:car, :person, :dealership, :salesperson).all
+      .joins(:dealership)
+      .order('dealerships.name ASC')
+      .page params[:page]
   end
 end

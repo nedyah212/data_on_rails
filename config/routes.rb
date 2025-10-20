e88ecf7553d8d25be8912c8717e29d1bd35ec607
nedyah212 <hploszay2@academic.rrc.ca>
2025-10-20 08:56:06 -0500

    fixed copy and pasted table link that caused a validation error -- out of place table data tag

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :cities, only: [:index, :show]
   resources :manufacturers, only: [:index, :show]
   resources :cars, only: [:index, :show]
+  resources :cities, only: [:show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
